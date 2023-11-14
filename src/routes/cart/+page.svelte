50339b0e54ff1500711f4b7a2829232e64f1f2ef
hehe <jasonwillcond@gmail.com>
2023-11-14 22:37:18 +0700

    finish

--- src/routes/cart/+page.svelte
+++ src/routes/cart/+page.svelte
@@ -65,7 +65,7 @@
         });
 
         const content = await response.json();
-        openModal('Error', content.message);
+        openModal('Success', content.message);
 
         await loadItems();
     }
